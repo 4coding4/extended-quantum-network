3f0e0e106113089cbfd54cb429d78a2d946cd6ef
Edoardo Riggio <edo.riggio19@gmail.com>
2022-12-08 16:11:16 +0100

    added example back

--- src/models/DynamicFibreDelay.py
+++ src/models/DynamicFibreDelay.py
@@ -3,23 +3,22 @@ from netsquid.components import FibreDelayModel
 
 class DynamicFibreDelayModel:
     """
-    Transmission delay model based on constant speed of photons through fibre.
-
-    The travel distance is given by length of channel.
+    Transmission delay model based on constant speed of photons through fibre. The travel distance is given by length
+    of channel.
 
     Notes
     -----
-    The default c refers to the speed of light in a f1 fibre cable, which is 200000 km/s,
-    ~30% slower than the speed of light in a vacuum.
+    The default c refers to the speed of light in a f1 fibre cable, which is 200000 km/s, ~30% slower than the speed of
+    light in a vacuum.
 
     Model parameters
     ----------------
     c:
         (float, optional) – Fixed speed of photons through the channel [km/s].
     """
-
     _c: float = 200000
     _fibre_delay_model: FibreDelayModel = None
+
 
     def __init__(self):
         self.fibre_delay_model: FibreDelayModel = FibreDelayModel(self._c)
