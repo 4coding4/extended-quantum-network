f7788a5a4fc73dca495c46a4e8fb78a3fe8de497
4coding4 <36158335+4coding4@users.noreply.github.com>
2022-12-13 22:10:00 +0100

    created Combined class
    to connect all models to QuantumChannel using only 1 command

---
+++ src/models/Combined.py
@@ -0,0 +1,20 @@
+from src.models.DynamicFibreDelay import DynamicFibreDelay
+from src.models.FibreError import FibreError
+from src.models.T1T2Error import T1T2Error
+
+
+class Combined:
+    """
+    This class is used to combine the 3 models (with default parameters) in order to import them into the network directly.
+    Instructions:
+        1.Import this class into the network
+        from src.models.Combined import Combined
+        2. Connect the models to the network (in specific to the QuantumChannel).
+        models = Combined.models
+    """
+    # Create a dictionary of models for the models parameter of QuantumChannel
+    models: dict = dict(
+        quantum_loss_model=FibreError.loss_model,
+        quantum_noise_model=T1T2Error.noise_model,
+        quantum_delay_model=DynamicFibreDelay.fibre_delay_model
+    )
