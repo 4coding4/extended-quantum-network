b81a36ea90586d36db97188560dde83fb45120c0
Edoardo Riggio <edo.riggio19@gmail.com>
2022-12-20 17:08:50 +0100

    minor

--- src/main.py
+++ src/main.py
@@ -7,15 +7,23 @@ if __name__ == "__main__":
     # Initialize Network and run experiment
     star_network: StarNetwork = StarNetwork(Combined.models)
 
-    # # Run experiment (Node1 *-* Node2)
+
+    # Run single experiment
+    # ---------------------
+    fidelity = star_network.entangle_nodes(1, 3)
+    # fidelity = star_network.entangle_nodes(1, 4)
+    print(fidelity)
+
+    # Run experiment suite (Node1 *-* Node2)
+    # --------------------------------------
     # experiment: Experiment = Experiment(star_network, verbose=False)
     # experiment.csv_path = "../out/data[node-node].csv"
     # experiment.fig_path = "../out/fidelity-over-length[node-node].png"
     # experiment.run(1, 3)
 
-    # Run experiment (Node1 *-* RemoteNode)
-    for i in range(11):
-        experiment: Experiment = Experiment(star_network, verbose=False)
-        experiment.csv_path = f"../out/data[node-remote]-{i}.csv"
-        experiment.fig_path = f"../out/fidelity-over-length[node-remote]-{i}.png"
-        experiment.run(1, 4)
+    # Run experiment suite (Node1 *-* RemoteNode)
+    # -------------------------------------------
+    # experiment: Experiment = Experiment(star_network, verbose=False)
+    # experiment.csv_path = f"../out/data[node-remote].csv"
+    # experiment.fig_path = f"../out/fidelity-over-length[node-remote].png"
+    # experiment.run(1, 4)
