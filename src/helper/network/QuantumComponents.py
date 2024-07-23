2ad5260f39aeada43ca4d95399dc87f7ee525d28
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-07-23 16:35:20 +0200

    remove old get_source in QuantumComponents

--- src/helper/network/QuantumComponents.py
+++ src/helper/network/QuantumComponents.py
@@ -1,7 +1,4 @@
 from netsquid.components import QuantumProcessor
-from netsquid import b00
-from netsquid.components import QSource, SourceStatus, FixedDelayModel
-from netsquid.qubits import StateSampler
 
 
 class QuantumComponents:
@@ -18,15 +15,3 @@ class QuantumComponents:
                                 num_positions=mem_positions,
                                 fallback_to_nonphysical=True
                                 )
-
-    @staticmethod
-    def get_source(name, delay, num_ports):
-        """
-        Get QSource component with default settings.
-        """
-        return QSource(name,
-                       state_sampler=StateSampler([b00]),
-                       status=SourceStatus.EXTERNAL,
-                       models={"emission_delay_model": FixedDelayModel(delay=delay)},
-                       num_ports=num_ports
-                       )
