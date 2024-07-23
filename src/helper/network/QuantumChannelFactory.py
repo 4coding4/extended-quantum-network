31173714d6d40bd20b008fa428bf473af58d873b
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-07-23 15:48:33 +0200

    Create QuantumChannelFactory.py
    in helper/network/

---
+++ src/helper/network/QuantumChannelFactory.py
@@ -0,0 +1,40 @@
+from netsquid.components import QuantumChannel
+
+
+class QuantumChannelFactory:
+    """
+    Factory class for creating quantum channels.
+    """
+
+    def __init__(self, length, models):
+        """
+        Create a quantum channel factory.
+
+        Parameters
+        ----------
+        length:
+            (float) – Length of the channel [km].
+        models:
+            (dict) – Dictionary of models to use for the channel.
+        """
+        self.length = length
+        self.models = models
+
+    def get_channel(self, name):
+        """
+        Get a quantum channel with the given name.
+
+        Parameters
+        ----------
+        name:
+            (str) – Name of the channel.
+
+        Returns
+        -------
+        QuantumChannel
+            Quantum channel with the given name.
+        """
+        return QuantumChannel(name,
+                              length=self.length,
+                              models=self.models
+                              )
