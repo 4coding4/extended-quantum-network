5a42049fb422d8847564d90d32c5e41c25d8cb51
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-07-23 14:14:16 +0200

    move MemorySnapshot
    in helper/network/

--- src/network/StarNetwork.py
+++ src/network/StarNetwork.py
@@ -5,7 +5,7 @@ from netsquid.nodes import Network, node
 from netsquid.qubits import QRepr
 from typing import Tuple, List, Dict, Union
 
-from src.network.MemorySnapshot import MemorySnapshot
+from src.helper.network.MemorySnapshot import MemorySnapshot
 from src.helper.network.PortPair import PortPair
 from src.helper.network.QuantumComponents import QuantumComponents as QC
 from src.protocols.GenerateEntanglement import GenerateEntanglement
