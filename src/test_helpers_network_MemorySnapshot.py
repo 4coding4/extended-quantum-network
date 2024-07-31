d52d0ca3241aacc1022c2c644a8fbf3656253b5a
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-07-31 16:42:34 +0200

    fixed by replacing Starnetwork w brand new network from scratch

--- src/test_helpers_network_MemorySnapshot.py
+++ src/test_helpers_network_MemorySnapshot.py
@@ -1,26 +1,52 @@
 import unittest
 
+from netsquid import sim_reset
+from netsquid.nodes import Network
+
+from src.helper.network.Factory.QuantumProcessor import QuantumProcessorFactory
 from src.helper.network.MemorySnapshot import MemorySnapshot
 from src.network.StarNetwork import StarNetwork
 
 
 class TestHelpersNetworkMemorySnapshot(unittest.TestCase):
-    # test w default values in protocol a
-    star_network = StarNetwork({})
-    node1: int = 1
-    node2: int = 2
-    node3: int = 4
-    debug: bool = True
-    repeater_mem_positions: int = 4
-    node_mem_positions: int = 1
-    remote_node_mem_positions: int = 2
-    memory_snapshot = MemorySnapshot(star_network.network, node1, node2, node3,
-                                     repeater_mem_positions,
-                                     node_mem_positions,
-                                     remote_node_mem_positions)
+    sim_reset()
 
     def test_show_all_memory_positions(self):
-        msg = self.memory_snapshot.show_all_memory_positions()
+        # test w default values in protocol a
+        node1: int = 1
+        node2: int = 2
+        node3: int = 4
+        repeater_mem_positions: int = 4
+        node_mem_positions: int = 1
+        remote_node_mem_positions: int = 2
+
+        # create a network
+        network = Network("Network")
+        # add nodes to the network
+        network.add_node("Node1")
+        network.add_node("Node2")
+        network.add_node("Repeater")
+        network.add_node("RemoteNode")
+        # subcomponents processor w memories
+        quantum_processor_factory = QuantumProcessorFactory()
+        network.nodes["Repeater"].add_subcomponent(
+            quantum_processor_factory.get("QP_Repeater", repeater_mem_positions)
+        )
+        network.nodes["RemoteNode"].add_subcomponent(
+            quantum_processor_factory.get("QP_RemoteNode", remote_node_mem_positions)
+        )
+        network.nodes["Node1"].add_subcomponent(
+            quantum_processor_factory.get(f"QP_Node1", node_mem_positions)
+        )
+        network.nodes["Node2"].add_subcomponent(
+            quantum_processor_factory.get(f"QP_Node2", node_mem_positions)
+        )
+
+        memory_snapshot = MemorySnapshot(network, node1, node2, node3,
+                                         repeater_mem_positions,
+                                         node_mem_positions,
+                                         remote_node_mem_positions)
+        msg = memory_snapshot.show_all_memory_positions()
         expected = ("--------------------------------------------------------------------------------\nRepeater_m0: "
                     "None\nRepeater_m1: None\nRepeater_m2: None\nRepeater_m3: None\nNode1_m0: None\nNode2_m0: "
                     "None\nRemoteNode_m0: None\nRemoteNode_m1: NoneIf it is working correctly, the output should have "
