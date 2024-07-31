a5f405ec0db6ef1296e449fe999a8f78ffeae3af
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-07-31 11:35:13 +0200

    mod print_bell_measurement to return msg
    used for testing

--- src/helper/network/entanglement_swapping_utils/bell_measurement.py
+++ src/helper/network/entanglement_swapping_utils/bell_measurement.py
@@ -17,13 +17,16 @@ def perform_bell_measurement(remote_node_memory, positions: list = []):
     return m
 
 
-def print_bell_measurement(m, state):
+def print_bell_measurement(m, state) -> str:
     """
     Print the measurement of the Bell state and the state of the qubit in the Repeater
     :param m: The measurement of the Bell state
     :param state: The state of the qubit in the Repeater
+    :return: The message with the measurement of the Bell state and the state of the qubit in the Repeater
     """
-    print('Bell measurement in repeater:')
-    print('m= ', m, ', state:')
-    print('M/Indices format for the states: ', state)
-    print('B/Bell states/Ket vectors format for the states: ', ketstates.BellIndex(state))
+    msg = 'Bell measurement in repeater:\n'
+    msg += f'm= {m}, state:\n'
+    msg += f'M/Indices format for the states: {state}\n'
+    msg += f'B/Bell states/Ket vectors format for the states: {ketstates.BellIndex(state)}\n'
+    print(msg)
+    return msg
