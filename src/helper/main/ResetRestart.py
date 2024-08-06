b5aac4aaf9b200041c673d2ea2416ce59a76b4d9
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-06 11:49:46 +0200

    fix usages

--- src/helper/main/ResetRestart.py
+++ src/helper/main/ResetRestart.py
@@ -1,6 +1,8 @@
 from netsquid import sim_reset
 import subprocess
 
+from src.terminal.terminal_utils import show_output_terminal, run_command_in_terminal_and_show_output
+
 
 def reset_simulation() -> None:
     """Reset the simulation."""
@@ -9,30 +11,24 @@ def reset_simulation() -> None:
 
 def restart_simulation(show_output: bool = False, debug: bool = False) -> None:
     """Restart the simulation."""
-
-    def show_output_terminal(init_msg: str, cmd: subprocess.CompletedProcess, last_msg: str = "\nExit code: ") -> None:
-        """
-        Show the output of the command in the terminal;
-        it will print the following: the message before, the standard output, the message after, and the exit code.
-        :param init_msg: The message to print before the output of the command
-        :param cmd: The CompletedProcess of the command that has been run
-        :param last_msg: The message before the exit code and after the output of the command
-        """
-        print(init_msg, cmd.stdout, last_msg, cmd.returncode)
-
     # open the terminal and show the current directory and the files in it
     if debug:
-        current_directory = subprocess.run(["pwd"], stdout=subprocess.PIPE, text=True)
-        show_output_terminal("Current directory: ", current_directory)
-        files = subprocess.run(["ls", "-la"], stdout=subprocess.PIPE, text=True)
-        show_output_terminal("Files in directory: ", files)
+        flags = ["text", "stdout"]
+        pwd = run_command_in_terminal_and_show_output(["pwd"], flags, "Current directory: ")
+        # current_directory = subprocess.run(["pwd"], stdout=subprocess.PIPE, text=True)
+        # show_output_terminal("Current directory: ", current_directory)
+        # files = subprocess.run(["ls", "-la"], stdout=subprocess.PIPE, text=True)
+        # show_output_terminal("Files in directory: ", files)
+        files = run_command_in_terminal_and_show_output(["ls", "-la"], flags, "Files in directory: ")
     # open the terminal and run the program again
-    program = subprocess.run(["python", "main.py"])
-    if show_output:
-        show_output_terminal("Standard output of the program: ", program)
+    # program = subprocess.run(["python", "main.py"])
+    # if show_output:
+    #     show_output_terminal("Standard output of the program: ", program)
+    program = run_command_in_terminal_and_show_output(["python", "main.py"], [], "Standard output of the program: ")
+    return program
 
 
-def reset_and_restart_simulation() -> None:
+def reset_and_restart_simulation(debug: bool = False) -> None:
     """Reset and Restart the simulation."""
     reset_simulation()
     restart_simulation()
