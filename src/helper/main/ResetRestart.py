4fe9e801ead098ce856285bd6a3a7868b4a995a1
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-06 11:52:35 +0200

    cleanup ResetRestart

--- src/helper/main/ResetRestart.py
+++ src/helper/main/ResetRestart.py
@@ -1,7 +1,6 @@
 from netsquid import sim_reset
-import subprocess
 
-from src.terminal.terminal_utils import show_output_terminal, run_command_in_terminal_and_show_output
+from src.terminal.terminal_utils import run_command_in_terminal_and_show_output
 
 
 def reset_simulation() -> None:
@@ -9,21 +8,16 @@ def reset_simulation() -> None:
     sim_reset()
 
 
-def restart_simulation(show_output: bool = False, debug: bool = False) -> None:
-    """Restart the simulation."""
-    # open the terminal and show the current directory and the files in it
+def restart_simulation(debug: bool = False) -> str:
+    """Restart the simulation.
+    :param debug: The flag to enable the debug mode (default: False)
+    :return: The output of the program
+    """
     if debug:
         flags = ["text", "stdout"]
-        pwd = run_command_in_terminal_and_show_output(["pwd"], flags, "Current directory: ")
-        # current_directory = subprocess.run(["pwd"], stdout=subprocess.PIPE, text=True)
-        # show_output_terminal("Current directory: ", current_directory)
-        # files = subprocess.run(["ls", "-la"], stdout=subprocess.PIPE, text=True)
-        # show_output_terminal("Files in directory: ", files)
-        files = run_command_in_terminal_and_show_output(["ls", "-la"], flags, "Files in directory: ")
-    # open the terminal and run the program again
-    # program = subprocess.run(["python", "main.py"])
-    # if show_output:
-    #     show_output_terminal("Standard output of the program: ", program)
+        run_command_in_terminal_and_show_output(["pwd"], flags, "Current directory: ")
+        run_command_in_terminal_and_show_output(["ls", "-la"], flags, "Files in directory: ")
+
     program = run_command_in_terminal_and_show_output(["python", "main.py"], [], "Standard output of the program: ")
     return program
 
@@ -31,11 +25,14 @@ def restart_simulation(show_output: bool = False, debug: bool = False) -> None:
 def reset_and_restart_simulation(debug: bool = False) -> None:
     """Reset and Restart the simulation."""
     reset_simulation()
-    restart_simulation()
+    restart_simulation(debug)
 
 
 def check_reset_restart(flag: bool) -> bool:
-    """Check if the reset and restart is required."""
+    """Check if the reset and restart is required.
+    :param flag: The flag to check if the reset and restart is required
+    :return: The flag, if it is False, the simulation will not be reset and restarted
+    """
     if flag:
         reset_and_restart_simulation()
     # return the flag, if it is False, the simulation will not be reset and restarted
