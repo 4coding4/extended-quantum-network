0e64002a2c753f9917fe6a0400f081567bbb3397
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-06 16:13:29 +0200

    skip 100% and no code files in report
    both in text terminal and html

--- shell_scripts/coverage.sh
+++ shell_scripts/coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 coverage run --parallel-mode --source=./ -m unittest -v
 coverage combine
-coverage report -m
-coverage html
+coverage report -m --skip-covered --skip-empty
+coverage html --skip-covered --skip-empty
 #open htmlcov/index.html
 open http://localhost:63342/quantum-network/htmlcov/index.html
