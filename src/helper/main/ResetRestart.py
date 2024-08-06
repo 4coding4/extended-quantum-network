e466f40e0d8f0e4553629724b97b5ac013b75aa0
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-06 12:16:36 +0200

    mod out reset_and_restart_simulation and docstrings

--- src/helper/main/ResetRestart.py
+++ src/helper/main/ResetRestart.py
@@ -9,7 +9,8 @@ def reset_simulation() -> None:
 
 
 def restart_simulation(debug: bool = False) -> str:
-    """Restart the simulation.
+    """
+    Restart the simulation.
     :param debug: The flag to enable the debug mode (default: False)
     :return: The output of the program
     """
@@ -22,14 +23,20 @@ def restart_simulation(debug: bool = False) -> str:
     return program
 
 
-def reset_and_restart_simulation(debug: bool = False) -> None:
-    """Reset and Restart the simulation."""
+def reset_and_restart_simulation(debug: bool = False) -> str:
+    """
+    Reset and Restart the simulation.
+    :param debug: The flag to enable the debug mode (default: False)
+    :return: The output of the program
+    """
     reset_simulation()
-    restart_simulation(debug)
+    out = restart_simulation(debug)
+    return out
 
 
 def check_reset_restart(flag: bool) -> bool:
-    """Check if the reset and restart is required.
+    """
+    Check if the reset and restart is required.
     :param flag: The flag to check if the reset and restart is required
     :return: The flag, if it is False, the simulation will not be reset and restarted
     """
