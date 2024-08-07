37ca7d56f9525088c3d389aefbc96fc446ad5d3c
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-07 11:12:19 +0200

    mod and more strict test_reset_and_restart_simulation

--- src/test_helpers_main_ResetRestart.py
+++ src/test_helpers_main_ResetRestart.py
@@ -4,6 +4,8 @@ from src.helper.main.ResetRestart import reset_and_restart_simulation, check_res
 
 
 class TestHelpersMainResetRestart(unittest.TestCase):
+    # Needed to fix the path of the main.py in restart_simulation function, when run all the tests
+    # (but not the single test/class)
     debug = True
 
     def test_reset_and_restart_simulation(self):
@@ -12,10 +14,14 @@ class TestHelpersMainResetRestart(unittest.TestCase):
         # out starts with"Standard output of the program: "
         self.assertTrue(out.startswith("Standard output of the program: "))
         # out contains the followings:
-        # ...
+        self.assertIn("Parsed arguments:", out)
+        self.assertIn("If it is working correctly, the output should have 4 Qubits and 4 None", out)
+        self.assertIn("If it is working correctly, the output should have all Qubits and 0 None", out)
+        self.assertIn("Bell measurement in repeater:", out)
+        self.assertIn("After entanglement swapping:", out)
+        self.assertIn("If it is working correctly, the output should have all None", out)
         # out ends with "Exit code: "
-        # self.assertTrue(out.endswith("Exit code: "))
-        print("out:", out)
+        self.assertTrue(out.endswith("Exit code: 0"))
 
     def test_check_reset_restart(self):
         new_flag = check_reset_restart(True, self.debug)
