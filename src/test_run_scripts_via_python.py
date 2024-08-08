fce492491842c41154e76edceac1dab4d7195e8d
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-08 11:26:04 +0200

    added 1 test

--- src/test_run_scripts_via_python.py
+++ src/test_run_scripts_via_python.py
@@ -4,10 +4,15 @@ from run_scripts_via_python import run_bash_and_return_outputs
 
 
 class TestRunScriptsViaPython(unittest.TestCase):
-    def test_run_bash_script(self):
+    def test_run_code_lint(self):
         bash_name = "code_lint.sh"
         out = run_bash_and_return_outputs(bash_name)
         self.assertIn("bash run:", out)
         self.assertIn("Exit code: 1", out)
+
+    def test_run_generate_req(self):
+        bash_name = "generate_req.sh"
+        out = run_bash_and_return_outputs(bash_name, [])  # no flags needed
+        self.assertEqual('bash run: \n\nNone\n\nExit code: 0', out)
 
 # via this, the function above cannot file the bash script, but by running coverage.sh it works as expected
