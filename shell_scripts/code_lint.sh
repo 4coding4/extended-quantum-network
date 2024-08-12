5aa8811510d801aef93978891f154942fd8c16ae
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-12 11:59:39 +0200

    rm old reports before running new one

--- shell_scripts/code_lint.sh
+++ shell_scripts/code_lint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #flake8 --max-line-length=127
 #flake8 --max-line-length=127 --statistics
+rm /opt/project/reports/flake/flake8stats.txt
 flake8 /opt/project/src/ --max-line-length=127 --exit-zero --statistics --tee --output-file /opt/project/reports/flake/flake8stats.txt
--- shell_scripts/coverage.sh
+++ shell_scripts/coverage.sh
@@ -3,6 +3,7 @@ coverage run --parallel-mode --source=./ -m unittest -v
 coverage combine
 coverage report
 #-m --skip-covered --skip-empty
+rm /opt/project/reports/coverage/coverage.xml
 coverage xml -o /opt/project/reports/coverage/coverage.xml
 coverage html
 #--skip-covered --skip-empty
