eaf630dbbbff385d7c453db740e36101e5eafeb4
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-12 11:45:43 +0200

    fix flake8 and gen report

--- shell_scripts/code_lint.sh
+++ shell_scripts/code_lint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #flake8 --max-line-length=127
 flake8 --max-line-length=127 --statistics
+flake8 /opt/project/src/ --max-line-length=127 --exit-zero --statistics --tee --output-file /opt/project/reports/flake/flake8stats.txt
