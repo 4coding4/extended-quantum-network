9e3940060b612a29c79494a5a5e59084f53779a0
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-12 11:26:48 +0200

    Update coverage.sh

--- shell_scripts/coverage.sh
+++ shell_scripts/coverage.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 coverage run --parallel-mode --source=./ -m unittest -v
 coverage combine
-coverage report -m --skip-covered --skip-empty
-coverage html --skip-covered --skip-empty
+coverage report
+#-m --skip-covered --skip-empty
+coverage xml
+coverage html
+#--skip-covered --skip-empty
 #open htmlcov/index.html
 open http://localhost:63342/quantum-network/htmlcov/index.html
