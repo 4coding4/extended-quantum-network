58b3d7c5f7e495d253a3b1f4498b164f2aed9696
4coding4 <36158335+4coding4@users.noreply.github.com>
2024-08-16 16:10:19 +0200

    update all the reports by running the other scripts before generate_badges

--- shell_scripts/generate_badges.sh
+++ shell_scripts/generate_badges.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# update all the reports by running the other scripts
+/opt/project/shell_scripts/test_all.sh
+/opt/project/shell_scripts/coverage.sh
+/opt/project/shell_scripts/code_lint.sh
+
 genbadge tests -o '/opt/project/badges/tests-badge.svg'
 genbadge coverage -o '/opt/project/badges/coverage-badge.svg'
 genbadge flake8 -o '/opt/project/badges/flake8-badge.svg'
